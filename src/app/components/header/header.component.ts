a7d93955f45dc1cca8019701b5f01aa2845270b2
gashaw mucheye <gashaw2533@gmail.com>
2023-10-06 10:16:14 +0200

    creating route and footer component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription!: Subscription;
+  constructor(private uiservice: UiService, private router: Router) {
+    this.subscription = this.uiservice
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
   toggleAddTask() {
-    alert('toggle');
+    this.uiservice.toggleAddTask();
+  }
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
