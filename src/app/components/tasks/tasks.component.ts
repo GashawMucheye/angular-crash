d9370496a0a09bf9b6239c37569655c990647dd6
gashaw mucheye <gashaw2533@gmail.com>
2023-10-06 02:48:18 +0200

    adding component add-task and adding styling and create fake rest api by json-server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
 import { Task } from 'src/app/Task';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +8,22 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
-  tasks: Array<Task> = TASKS;
+  tasks: Array<Task> = [];
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
 }
